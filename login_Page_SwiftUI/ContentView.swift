800657c82df9fdda55020c86166405d13aacba1b
Lex-ios-Stack <84474579+Lexstack@users.noreply.github.com>
2021-06-10 08:42:50 -0400

    Initial Commit

---
+++ login_Page_SwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  login_Page_SwiftUI
+//
+//  Created by Field Employee on 6/10/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
